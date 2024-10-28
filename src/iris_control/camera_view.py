36960a52a48d0b7af184a04e7a5da26db630c846
lsriw <nowosad@student.agh.edu.pl>
2024-10-28 16:04:40 +0100

    Changed camera_view and models textures

--- src/iris_control/camera_view.py
+++ src/iris_control/camera_view.py
@@ -8,9 +8,48 @@ import numpy as np
 def camera_sub_callback(msg: Image):
     frame = np.array(list(msg.data)).astype(np.uint8)
     frame = np.reshape(frame, (msg.height, msg.width, 3))
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    # frame = np.where()
 
-    cv2.imshow("drone_camera", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    marker_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+    param_markers  = cv2.aruco.DetectorParameters_create()
+    
+
+    corners, ids, reject = cv2.aruco.detectMarkers(
+        gray_frame, marker_dict, parameters = param_markers
+    )
+    # print(marker_corners)
+    # corner1, corner2, corner3, corner4 = [marker[i][0] for i, marker in enumerate(marker_corners)]
+    if len(corners) > 0:
+        # flatten the ArUco IDs list
+        ids = ids.flatten()
+        # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+            # extract the marker corners (which are always returned in
+            # top-left, top-right, bottom-right, and bottom-left order)
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            # draw the bounding box of the ArUCo detection
+            cv2.line(gray_frame, topLeft, topRight, (0, 255, 0), 2)
+            cv2.line(gray_frame, topRight, bottomRight, (0, 255, 0), 2)
+            cv2.line(gray_frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+            cv2.line(gray_frame, bottomLeft, topLeft, (0, 255, 0), 2)
+            # compute and draw the center (x, y)-coordinates of the ArUco
+            # marker
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            cv2.circle(gray_frame, (cX, cY), 4, (0, 0, 255), -1)
+            # draw the ArUco marker ID on the image
+            cv2.putText(gray_frame, str(markerID),
+                (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (0, 255, 0), 2)
+            
+    cv2.imshow("test", gray_frame)
     cv2.waitKey(1)
 
 
