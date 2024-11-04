cc7a0d93e6074dabb3b8b0a76185befba8d0f6bb
Roman Nowak <roman.nowak9@interia.pl>
2024-11-04 16:47:57 +0100

    control procedure

--- src/iris_control/position_control.py
+++ src/iris_control/position_control.py
@@ -3,20 +3,23 @@
 import rospy
 from std_msgs.msg import Bool
 from geometry_msgs.msg import PoseStamped
-from aruco_pose_estimation import estimate_pose
+from std_msgs.msg import UInt8
 from enum import Enum
 import numpy as np
 
-GATE_HEIGHT = 1  # TODO
-GATE_WIDTH = 1  # TODO
-MARKER_SIZE = 0.25  # TODO
-CAMERA_MATRIX = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # TODO
-CAMERA_DISTORTION = [0, 0, 0, 0, 0]  # TODO
 NO_GATES = 5  # TODO: Sprawdzić
 
+start_topic = "/iris_control/challenge_start"
+
 pos_pub_topic = "/mavros/setpoint_position/local"
+gate_pub_topic = "/iris/next_gate"
+
 pos_sub_topic = "/mavros/local_position/pose"
-pub = rospy.Publisher(pos_pub_topic, PoseStamped, queue_size=10)
+target_sub_topic = "/iris/target_pose"
+
+start_pub = rospy.Publisher(start_topic, Bool, queue_size=10)
+pos_pub = rospy.Publisher(pos_pub_topic, PoseStamped, queue_size=10)
+gate_pub = rospy.Publisher(gate_pub_topic, UInt8, queue_size=10)
 
 class Stage(Enum):
     STARTING = 1
@@ -26,7 +29,7 @@ class Stage(Enum):
 
 challenge_started = False
 challenge_stage  = Stage.STARTING
-current_marker_pose = PoseStamped()
+# current_marker_pose = PoseStamped()
 current_target = PoseStamped()
 next_gate = 1
 
@@ -40,13 +43,12 @@ def pose_cb(msg: PoseStamped):
 
         case Stage.STARTING:
             if challenge_started:
+                gate_msg = UInt8()
+                gate_msg.data = np.uint8(next_gate)
+                gate_pub.publish(gate_msg)
                 challenge_stage = Stage.FLIGHT
 
         case Stage.FLIGHT:
-            # Wykrywanie znacznika
-
-            # Jeśli wykryto oczekiwany znacznik, to liczymy jego pozycję i target
-
             # Dron przeleciał przez bramkę
             if np.round(drone_pose.x, 1) == np.round(current_target.x, 1) and\
                np.round(drone_pose.y, 1) == np.round(current_target.y, 1) and\
@@ -57,34 +59,50 @@ def pose_cb(msg: PoseStamped):
             
             if next_gate <= NO_GATES:
                 next_gate += 1
+                gate_msg = UInt8()
+                gate_msg.data = np.uint8(next_gate)
+                gate_pub.publish(gate_msg)
                 challenge_stage = Stage.FLIGHT
             else:
+                next_gate = 1
+                gate_msg = UInt8()
+                gate_msg.data = np.uint8(next_gate)
+                gate_pub.publish(gate_msg)
                 challenge_stage = Stage.FINISH
-
-
 
 
         case Stage.FINISH:
             # Koniec trasy - lądowanie
             current_target.pose.position.z = 0
+
+            # Koniec challengu
             challenge_started = False
+            start_msg = Bool
+            start_msg.data = challenge_started
+            start_pub.publish(start_msg)
         
 
-    pub.publish(current_target)
+    pos_pub.publish(current_target)
 
 
 def start_challenge_cb(msg: Bool):
     global challenge_started
     if msg.data:
         challenge_started = True
+        
+
+def target_cb(msg: PoseStamped):
+    global current_target
+    current_target = msg
 
 
 def drone_control():
-    rospy.init_node('talker', anonymous=True)
-    rospy.Subscriber("/iris_control/challenge_start", Bool, start_challenge_cb)
+    rospy.init_node('control', anonymous=True)
+    rospy.Subscriber(start_topic, Bool, start_challenge_cb)
 
     if challenge_started:
         rospy.Subscriber(pos_sub_topic, PoseStamped, pose_cb)
+        rospy.Subscriber(target_sub_topic, PoseStamped, target_cb)
     
     rospy.spin()
 
